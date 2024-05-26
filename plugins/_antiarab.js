489ab48c40266a8bce7857d0679ed1523198e246
Abdelraman Mohamed <131656995+shadow0192@users.noreply.github.com>
2024-05-26 09:00:29 +0300

    Update _antiarab.js

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -1,41 +1,33 @@
-// TheMystic-Bot-MD@BrunoSobrino - _antiarab.js
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
+let url = piees[Math.floor(Math.random() * piees.length)]
+conn.sendFile(m.chat, url, 'error.jpg', ` >_<...`, m)
+
+}
+handler.help = ['S H A D O W']
+handler.tags = ['S H A D O W']
+handler.command = /^(شعر)$/
+handler.exp = 50
+handler.level = 0
+export default handler
 
 
-const handler = (m) => m;
-handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-
-  const tradutor = _translate.plugins._antiarab
-  // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-  // To set the language, in the root of the project, modify the config.json file.
-
-  /* if (m.message) {
-    console.log(m.message)
-  }*/
-  if (!m.isGroup) return !1;
-  const chat = global.db.data.chats[m.chat];
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  if (isBotAdmin && chat.antiArab2 && !isAdmin && !isOwner && !isROwner && bot.restrict) {
-    if (m.sender.startsWith('212' || '212')) {
-      m.reply(tradutor.texto1);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    }
-
-    if (m.sender.startsWith('265' || '265')) {
-      m.reply(tradutor.texto2);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    }
-
-    if (m.sender.startsWith('92' || '92')) {
-      m.reply(tradutor.texto3);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    }
-  }
-};
-export default handler;
+global.piees =[
+  "https://audio.jukehost.co.uk/cFgTBq5smuONwUoZnBBP4jKy3D12OtKc",
+  "https://audio.jukehost.co.uk/Rx1uYvvuF7b1lJg1zwucHaOCbVi1dQyi",
+  "https://audio.jukehost.co.uk/cEDaa7SpNcJSXCaoa1DhRpyLglXEhIms",
+  "https://audio.jukehost.co.uk/fH73vhc5PXI3oCEOnqqo03G9rDK6iXfD",
+  "https://audio.jukehost.co.uk/7G2W7bgkPCa6wRkEKRgJ5Wv7b6FTPH02",
+  "https://audio.jukehost.co.uk/ESPWlVK7kvQstqkXTOwoiVtqG2X5T7tu",
+  "https://audio.jukehost.co.uk/ea9VqDy6eC7EHwCBxqtTokKBVzOdpL2R",
+  "https://audio.jukehost.co.uk/SErFliXRAV3iQntqBRhTH94u3vLApK7r",
+  "https://audio.jukehost.co.uk/ea9VqDy6eC7EHwCBxqtTokKBVzOdpL2R",
+  "https://audio.jukehost.co.uk/SErFliXRAV3iQntqBRhTH94u3vLApK7r",
+  "https://audio.jukehost.co.uk/ea9VqDy6eC7EHwCBxqtTokKBVzOdpL2R",
+  "https://audio.jukehost.co.uk/1IiYMCueOJFmCiPGbI1FGDcIvGh5ffRP",
+  "https://audio.jukehost.co.uk/oKBHAMqduvHJKZr0m4GahELoHXx222r3",
+  "https://audio.jukehost.co.uk/oKBHAMqduvHJKZr0m4GahELoHXx222r3",
+  "https://audio.jukehost.co.uk/dSiD4xXyoiArvVGIlbMJVNWnI5UM3CxR",
+  "https://audio.jukehost.co.uk/YMc2EfKTab3oKVHc5vSl84FdtLlBNbyq",
+  ];
